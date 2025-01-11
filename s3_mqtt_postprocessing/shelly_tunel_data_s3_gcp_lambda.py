60cb354852ee56363feb9bf7e023b17c6be51772
Oleg Mozharovskyi <Oleg_Mozharovskyi@epam.com>
2025-01-11 19:37:05 +0100

    [SHELLY-GCP]: add coments to code

--- s3_mqtt_postprocessing/shelly_tunel_data_s3_gcp_lambda.py
+++ s3_mqtt_postprocessing/shelly_tunel_data_s3_gcp_lambda.py
@@ -3,41 +3,53 @@ import logging
 import boto3
 from google.cloud import storage
 from google.oauth2 import service_account
-from google.oauth2.credentials import Credentials
 
 # Configure logger
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
 def get_gcp_access_secret():
+    # Define the secret name and the AWS region
     secret_name = "GCPTransferJobAccessTokenSecret"
     region_name = "eu-central-1"
+    # Create a Secrets Manager client
     session = boto3.session.Session()
     client = session.client(service_name='secretsmanager', region_name=region_name)
     try:
+        # Attempt to retrieve the secret value
         get_secret_value_response = client.get_secret_value(SecretId=secret_name)
     except Exception as exception_error:
+        # Log an error if the secret cannot be retrieved
         logger.error(f"Error retrieving secret: {exception_error}")
         return None
     else:
+        # Check if the secret contains a 'SecretString' and log the retrieval
         if 'SecretString' in get_secret_value_response:
             secret = get_secret_value_response['SecretString']
             logger.info(f"Retrieved GCP Access secret: {secret}")
             return json.loads(secret)
         else:
+            # Log an error if 'SecretString' is missing
             logger.error("Secret does not contain 'SecretString'")
             return None
 
 def lambda_handler(event, context):
     logger.info("Start of the function")
+    # Retrieve GCP credentials from the secret manager
     gcp_credentials = service_account.Credentials.from_service_account_info(get_gcp_access_secret())
-    logger.info(f'Retrived credentials: {gcp_credentials}')
+    logger.info(f'Retrieved credentials: {gcp_credentials}')
+    # Extract S3 bucket name and filenames from the event
     aws_s3bucket_name = event.get('s3bucket')
     filenames = event.get('filenames', [])
+    # Define the GCP bucket name
     gcp_bucket_name = 'mozharovskyi-shelly-gcp-agregated-mqtt-storage-eu'
+    # Create an S3 client
     s3_client = boto3.client('s3')
+    # Create a GCP Storage client
     gcp_storage_client = storage.Client(credentials=gcp_credentials)
     logger.info("GCP Storage Client created successfully")
+    # Access the GCP bucket
     bucket = gcp_storage_client.bucket(gcp_bucket_name)
     for filename in filenames:
         # Read file from S3
