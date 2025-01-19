fec2e6545a9f7044acfc1a8c90dadebff7265305
Oleg Mozharovskyi <Oleg_Mozharovskyi@epam.com>
2025-01-19 17:43:44 +0100

    [SHELLY-GCP]: create 3 zones comparation

--- gcp_analytics/03_big_query_all_days_by_hour.sql
+++ gcp_analytics/03_big_query_all_days_by_hour.sql
@@ -13,7 +13,7 @@ ORDER BY
 CREATE OR REPLACE TABLE `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._hourly_energy_consumption_kyiv` AS
 SELECT
   EXTRACT(HOUR FROM TIMESTAMP(minute, "Europe/Kiev")) AS hour_of_day_kyiv,
-  SUM(energy_kWh) AS total_energy_kWh
+  SUM(energy_Wh) AS total_energy_Wh
 FROM
   `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._energy_per_minute_with_zeros`
 GROUP BY
