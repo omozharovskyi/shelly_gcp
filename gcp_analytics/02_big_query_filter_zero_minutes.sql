f4805799b37e09f5bc98e70e785914e3e96be708
Oleg Mozharovskyi <Oleg_Mozharovskyi@epam.com>
2025-01-19 02:36:08 +0100

    [SHELLY-GCP]: readable join

--- gcp_analytics/02_big_query_filter_zero_minutes.sql
+++ gcp_analytics/02_big_query_filter_zero_minutes.sql
@@ -12,15 +12,15 @@ FROM
   time_range,
   UNNEST(GENERATE_TIMESTAMP_ARRAY(min_time, max_time, INTERVAL 1 MINUTE)) AS minute;
 
-CREATE OR REPLACE TABLE `shelly-gcp-data-analytics.bq_shelly_gcp_dataset.final_results` AS
+CREATE OR REPLACE TABLE `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._energy_per_minute_with_zeros` AS
 SELECT
-  a.minute,
-  IFNULL(e.energy_Wh, 0) AS energy_Wh
+  all_minutes.minute,
+  IFNULL(energy_minutes.energy_Wh, 0) AS energy_Wh
 FROM
-  `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._all_minutes` a
+  `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._all_minutes` all_minutes
 LEFT JOIN
-  `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._energy_per_minute` e
+  `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._energy_per_minute` energy_minutes
 ON
-  a.minute = e.minute
+  all_minutes.minute = energy_minutes.minute
 ORDER BY
-  a.minute;
+  all_minutes.minute;
