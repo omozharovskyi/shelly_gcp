baee13b55894e90aa5e942b03468262c83274194
Oleg Mozharovskyi <Oleg_Mozharovskyi@epam.com>
2025-01-19 02:09:51 +0100

    [SHELLY-GCP]: simplify sql

--- gcp_analytics/big_querry_agregate_to_minutes.sql
+++ gcp_analytics/big_querry_agregate_to_minutes.sql
@@ -1,4 +1,4 @@
-CREATE TEMP TABLE _minute_grouped AS
+CREATE OR REPLACE  TABLE `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._minute_grouped` AS
 SELECT
   TIMESTAMP_MILLIS(CAST(`timestamp` AS INT64)) AS timestamp,
   TIMESTAMP_TRUNC(TIMESTAMP_MILLIS(CAST(`timestamp` AS INT64)), MINUTE) AS minute,
@@ -8,33 +8,26 @@ FROM
 ORDER BY
   timestamp;
 
-CREATE TEMP TABLE _energy_per_minute AS
-WITH LeadValues AS (
-  SELECT
-    minute,
-    aprt_power,
-    LEAD(aprt_power) OVER (PARTITION BY minute ORDER BY timestamp) AS next_aprt_power,
-    timestamp,
-    LEAD(timestamp) OVER (PARTITION BY minute ORDER BY timestamp) AS next_timestamp
-  FROM
-    `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._minute_grouped`
-)
+CREATE OR REPLACE TABLE `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._lead_values` AS
+SELECT
+  minute,
+  aprt_power,
+  LEAD(aprt_power) OVER (PARTITION BY minute ORDER BY timestamp) AS next_aprt_power,
+  timestamp,
+  LEAD(timestamp) OVER (PARTITION BY minute ORDER BY timestamp) AS next_timestamp
+FROM
+  `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._minute_grouped`;
 
+CREATE OR REPLACE TABLE `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._energy_per_minute` AS
 SELECT
   minute,
   SUM(0.5 * (next_aprt_power + aprt_power) * TIMESTAMP_DIFF(next_timestamp, timestamp, SECOND) / 3600) / 1000 AS energy_kWh
 FROM
-  LeadValues
+  `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._lead_values`
 WHERE
   next_timestamp IS NOT NULL
 GROUP BY
   minute;
 
-CREATE TABLE `shelly-gcp-data-analytics.bq_shelly_gcp_dataset.final_results` AS
-SELECT
-  minute,
-  energy_kWh
-FROM
-  _energy_per_minute
-ORDER BY
-  minute;
+CREATE OR REPLACE TABLE `shelly-gcp-data-analytics.bq_shelly_gcp_dataset.final_results` AS
+SELECT * FROM `shelly-gcp-data-analytics.bq_shelly_gcp_dataset._energy_per_minute`;
